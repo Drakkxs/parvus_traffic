02abdab7a819bdeb72d9f7bb7279f52e004876e3
DRAKKKS <70417194+Drakkxs@users.noreply.github.com>
2025-11-22 10:03:11 -0500

    Removed setting baseAggression for constantly raising values

--- lua/ge/extensions/parvusTraffic.lua
+++ lua/ge/extensions/parvusTraffic.lua
@@ -9,6 +9,7 @@ local min = math.min
 local max = math.max
 local trafficAmount = gameplay_traffic.getTrafficAmount -- traffic amount
 local trafficData = gameplay_traffic.getTrafficData     -- traffic table data
+local trafficGetState = gameplay_traffic.getState       -- state
 
 -- when extension loaded
 local function onExtensionLoaded()
@@ -18,11 +19,9 @@ end
 -- when a vehicle is reset
 local function onVehicleResetted(id)
     -- Only During Traffic
-    if gameplay_traffic.getState() ~= "on" then return end
+    if trafficGetState() ~= "on" then return end
     local trafficVeh = trafficData()[id]
     if trafficVeh and trafficVeh.isAi then
-        local obj = getObjectByID(id)
-
         -- Past threshold the chances begin to drop
         local function probabilityWithinValue(value, startChance, decay, threshold)
             -- Get amount of active traffic vehicles
@@ -39,12 +38,13 @@ local function onVehicleResetted(id)
         end
 
         if random() < probabilityWithinValue(trafficAmount(true), 0.8, 0.05, 12) then
+            -- Vehicle Object
+            local obj = getObjectByID(id)
 
             -- Aggression
             local baseAggression = trafficVeh.vars.baseAggression or 0.35
             local aggression = min(max(random() * random() * 1.65 + 0.35, baseAggression), 2) -- lower skew between 0.35 and 2
             log('D', logTag, '(' .. id .. ') Set Aggression: (' .. aggression .. ')')
-            trafficVeh.vars.baseAggression = aggression
             obj:queueLuaCommand('ai.setAggression(' .. aggression .. ')')
 
             -- Tough traffic has higher damage limits
@@ -53,10 +53,11 @@ local function onVehicleResetted(id)
                 for i, v in ipairs(damageLimits) do
                     damageLimits[i] = floor(v * aggression)
                 end
-                log('D', logTag, '('.. id ..') Tougher Vehicle Spawned (DamageLimits='.. table.concat(damageLimits, " ") ..')')
+                log('D', logTag,
+                    '(' .. id .. ') Tougher Vehicle Spawned (DamageLimits=' .. table.concat(damageLimits, " ") .. ')')
             end
 
-            -- 
+            --
             if aggression > 1.5 and random() < probabilityWithinValue(trafficAmount(true), 0.8, 0.1, 12) then
                 -- 20% chance of outlaw if aggression is high
                 obj:queueLuaCommand('ai.setSpeedMode("off")')
