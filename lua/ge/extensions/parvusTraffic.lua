f21e5aa20ef4becee8973baf9d3344b455971260
Drakkxs <70417194+Drakkxs@users.noreply.github.com>
2025-11-26 17:03:00 -0500

    Added honk mechanics and cleanup feature

--- lua/ge/extensions/parvusTraffic.lua
+++ lua/ge/extensions/parvusTraffic.lua
@@ -1,7 +1,7 @@
 -- Aggression based traffic
 local M = {}
-M.dependencies = { 'gameplay_traffic' }
 local P = {}
+M.dependencies = { 'gameplay_traffic' }
 
 local logTag = 'parvusTraffic'
 
@@ -19,14 +19,22 @@ local trafficVars = gameplay_traffic.getTrafficVars     -- started
 
 -- [[DATA]]
 local trafficIdsSorted = {}
-local auxiliaryData = { -- additional data for traffic
-    queuedVehicle = 0   -- current traffic vehicle index
-}
+local auxiliaryData = {}
 
 -- [[SWITCHBOARDS]]
 local tAggresion = {}
 local tToughness = {}
 local tOutlaw = {}
+
+local auxiliaryData = {      -- additional data for traffic
+    queuedVehicle = 0,       -- current traffic vehicle index
+    queuedHonkedVehicle = 0, -- current vehicle that's been honked at tracker
+    lastHornState = {},
+
+    queuedInteraction = 0,   -- index for staggering the honkedInteractions list
+    honkedInteractions = {}, -- { {callerID, targetID}, {callerID, targetID}, ... }
+    vehData = {},            -- { 'targetID' = {obstructionCooldown = 0, obstructions = {{callerID, targetID}}}}
+}
 
 local function parvusTrafficResetAllBoards()
     tAggresion = { resolution = 2, skew = 3, baseAggression = 0.3, maxAggression = 2, startchance = 0.8, decay = 0.1, threshold = 2 }
@@ -40,53 +48,59 @@ local function onExtensionLoaded()
     log('D', logTag, "Extension Loaded")
 end
 
-function P:checkTargetVisible(self, targetId) -- checks if the other vehicle is visible (static raycast)
+function P.checkTargetVisible(id, targetId) -- checks if the other vehicle is visible (static raycast)
+    local veh = traffic()[id]
     local visible = false
     local targetVeh = targetId and traffic()[targetId]
     if targetVeh then
-        visible = self:checkRayCast(targetVeh.pos + vec3(0, 0, 1))
+        visible = veh:checkRayCast(targetVeh.pos + vec3(0, 0, 1))
     end
-    logTag('D', logTag, '(' .. targetID .. ') Is not visble to (' .. targetVeh .. ')')
+    log('D', logTag, '(' .. targetId .. ') Is not visble to (' .. id .. ')')
     return visible
 end
 
-function P:getTrafficInfront(pos, filters) -- filters are functions to test the traffic
+function P.getTrafficInfront(id, pos, distance, filters) -- filters are functions to test the traffic
+    local veh = traffic()[id]
     filters = filters or {}
-    -- If 'pos' is not provided, we use the self vehicle's position.
-    local callerPos = pos or self.pos
+    -- If 'pos' is not provided, we use the vehicle's position.
+    local callerPos = pos or veh.pos
 
     local bestId
     local bestDist = math.huge
+    -- convert the meter distance to systematic units
+    local maxDistLimitSq = distance and (distance * distance) or math.huge
 
     for _, targetID in ipairs(trafficIdsSorted) do
         local targetVeh = traffic()[targetID]
 
+        local distSq = callerPos:squaredDistance(targetVeh.pos)
+
+        if distSq > maxDistLimitSq then
+            goto continue_loop
+        end
+
         local required_check =
             (be:getObjectActive(targetID)) and
-            -- (P.checkTargetVisible(self, targetID)) and
-            (self.dirVec:dot(targetVeh.pos - self.pos) > 0)
+            (P.checkTargetVisible(id, targetID)) and
+            (veh.dirVec:dot(targetVeh.pos - veh.pos) > 0)
 
         if not required_check then
-            goto continue_loop -- Skip vehicles that are behind or inactive
+            goto continue_loop -- Skip vehicles that are behind or invisible
         end
 
         local passed_custom_filters = true
-
         -- The filters table is expected to contain functions { [1] = func1, [2] = func2, ...}
         for _, filter_func in ipairs(filters) do
-            if not filter_func(targetVeh) then
+            if not filter_func(targetVeh) or targetVeh.filter_func then
                 passed_custom_filters = false
                 break
             end
         end
 
         if passed_custom_filters then
-            local otherPos = targetVeh.pos
-            local dist = callerPos:squaredDistance(otherPos)
-
-            if dist < bestDist then
+            if distSq < bestDist then
                 bestId = targetID
-                bestDist = dist
+                bestDist = distSq
             end
         end
 
@@ -97,26 +111,149 @@ function P:getTrafficInfront(pos, filters) -- filters are functions to test the 
 end
 
 -- when horn electronic is true
-function P:parvusTrafficHonkHorn()
-    log('D', logTag, '(' .. self.id .. ') Vehicle is honking')
-    local targetId, dist = P.getTrafficInfront(self, self.pos)
+function P.parvusTrafficHonkHorn(id)
+    local veh = traffic()[id]
+    log('D', logTag, '(' .. id .. ') Vehicle is honking')
+
+    -- Get the current speed (in m/s)
+    local speed = veh.speed or 0
+
+    -- dynamic honking distance (D = min(max(V * 2, 10), 50))
+    local dynaDist = math.min(math.max(speed * 2, 30), 60)
+    local targetId, dist = P.getTrafficInfront(id, veh.pos, dynaDist)
+
     if not targetId then return end
-    log('D', logTag, 'Target: (' .. targetId .. ') Distance: (' .. dist .. ')')
+    log('D', logTag, 'Target: (' .. targetId .. ') Distance: (' .. dist .. ') HonkDistance: (' .. dynaDist .. ')')
+
+    table.insert(auxiliaryData.honkedInteractions, { id, targetId })
 end
 
 -- begins tracking vehicle
-function P:parvusTrafficTracking()
-    local objMap = map and map.objects[self.id]
+function P.parvusTrafficTracking(id)
+    if not auxiliaryData.vehData[id] then auxiliaryData.vehData[id] = {} end
+    local vehData = auxiliaryData.vehData[id]
+    if not vehData.obstructionCooldown then vehData.obstructionCooldown = 0 end
+    if not vehData.obstructions then vehData.obstructions = {} end
+    if not vehData.lastObstuctionPos then
+        vec3()
+        vehData.lastObstuctionPos = traffic()[id].pos
+    end
+
+    local objMap = map and map.objects[id]
     if not objMap or not objMap.states then return end
 
-    -- honkState
-    local horn = objMap.states.horn and true or false
-    if horn then P.parvusTrafficHonkHorn(self) end
+    local currentHorn = objMap.states.horn and true or false
+
+    local prevHorn = auxiliaryData.lastHornState[id] or false
+
+    -- horn just turned on
+    if currentHorn and not prevHorn then
+        P.parvusTrafficHonkHorn(id)
+    end
+
+    -- the current state for the next check
+    auxiliaryData.lastHornState[id] = currentHorn
+    if vehData.obstructionCooldown > 0 then vehData.obstructionCooldown = vehData.obstructionCooldown - 1 end
+end
+
+function P.processHonkedAtVehicles(callerID, targetID)
+    local targetVeh = traffic()[targetID]
+    if targetVeh then
+        local vehData = auxiliaryData.vehData[targetID]
+        local function modifyCooldown(v, add)
+            if not v then return end
+            v.obstructionCooldown = v.obstructionCooldown + (add or 0)
+        end
+        if vehData and vehData.obstructionCooldown > 0 then return end
+        modifyCooldown(vehData, 10) -- cooldown
+
+        log('D', logTag, 'Honk Process: Caller (' .. callerID .. ')  Target:  (' .. targetID .. ')')
+
+        if not targetVeh.isAi then
+            log('D', logTag, '(' .. targetID .. ') Not AI')
+            return
+        end
+
+        -- Get the current speed (in m/s)
+        local speed = targetVeh.speed or 0
+
+        if targetVeh and speed > 2 then
+            log('D', logTag, '(' .. targetID .. ') Is Moving')
+            return
+        end
+
+        -- honk incase a vehicle is infront of this one
+        targetVeh.queuedFuncs.parvusTrafficHonk = {
+            timer = min(1, square(random())),
+            func = function()
+                if targetVeh then targetVeh:honkHorn(max(0.25, square(random()))) end
+            end,
+            args = {}
+        }
+
+        -- reset obstructions when vehicle moves
+        if targetVeh.pos:squaredDistance(vehData.lastObstuctionPos) > square(5) then
+            log('D', logTag, '(' .. targetID .. ') Target Moved, clearing obstructions')
+            table.clear(vehData.obstructions)
+            -- update position
+            vehData.lastObstuctionPos = targetVeh.pos
+        end
+
+        table.insert(vehData.obstructions, { callerID, targetID })
+        local strikes = #vehData.obstructions
+
+
+        if strikes > 3 then
+            targetVeh.queuedFuncs.parvusTrafficRemoveStuck = {
+                timer = 10.00,
+                func = function(id, lastPos, distance)
+                    local veh = traffic()[id]
+                    if veh and veh.speed < 2 and veh.pos:squaredDistance(lastPos) < distance then
+                        veh:modifyRespawnValues(-10)
+                        if veh.respawn.activeRadius > 80 then
+                            log('D', logTag, '(' .. id .. ') Vehicle is still active')
+                            return
+                        end
+                        veh:fade(5, true)
+                        log('D', logTag, '(' .. id .. ') Vehicle is still obstructing')
+                        return
+                    end
+                    log('D', logTag, '(' .. id .. ') Vehicle is no longer obstructing')
+                end,
+                args = { targetID, targetVeh.pos, 1 }
+            }
+            log('D', logTag, '(' .. targetID .. ') Obstruction Clear Queued')
+            targetVeh:resetAll()
+            table.clear(vehData.obstructions) -- clear strikes to allow other rules to try helping vehicle before it's removed
+            return
+        elseif strikes > 1 then
+            targetVeh.queuedFuncs.parvusTrafficSetAIRandom = {
+                timer = 0.25,
+                vLua = string.format('ai.setMode("random")') -- this is called to allow AI to make drastic efforts to get through
+            }
+            log('D', logTag, '(' .. targetID .. ') Queued AI to Random')
+
+            targetVeh.queuedFuncs.parvusTrafficSetAITraffic = {
+                timer = 2.25,
+                vLua = string.format('ai.setMode("traffic")')
+            }
+            log('D', logTag, '(' .. targetID .. ') Queued AI to Traffic')
+            return
+        elseif strikes > 0 then
+            targetVeh.queuedFuncs.parvusTrafficResetAI = {
+                timer = 0.25,
+                vLua = 'ai.reset()', -- this is called to reset the AI plan
+            }
+            log('D', logTag, '(' .. targetID .. ') Queued AI Rest')
+            return
+        end
+    end
 end
 
 -- updates every frame
 local function onUpdate(dt, dtSim)
     -- Only During Traffic
+    if trafficGetState() ~= "on" then return end
     if trafficGetState() ~= "on" then return end
     local vehCount = 0
     for i, id in ipairs(trafficIdsSorted) do -- ensures consistent order of vehicles
@@ -125,7 +262,7 @@ local function onUpdate(dt, dtSim)
         if veh then
             if be:getObjectActive(id) then
                 if i == auxiliaryData.queuedVehicle then -- checks one vehicle per frame, as an optimization
-                    P.parvusTrafficTracking(veh)
+                    P.parvusTrafficTracking(id)
                 end
             end
         end
@@ -135,59 +272,97 @@ local function onUpdate(dt, dtSim)
     if auxiliaryData.queuedVehicle > vehCount then
         auxiliaryData.queuedVehicle = 1
     end
+
+    local interactionCount = 0
+    for i, interaction in ipairs(auxiliaryData.honkedInteractions) do
+        interactionCount = interactionCount + 1
+        if interaction then
+            local callerID = interaction[1]
+            local targetID = interaction[2]
+
+            P.processHonkedAtVehicles(callerID, targetID)
+            table.remove(auxiliaryData.honkedInteractions, i)
+        end
+    end
+end
+
+
+-- Past threshold the chances begin to drop
+local function probabilityWithinValue(value, startChance, decay, threshold)
+    -- Get amount of active traffic vehicles
+    local N = value
+
+    local chance
+    if N <= threshold then
+        chance = startChance
+    else
+        chance = startChance / (1 + decay * (N - threshold))
+    end
+
+    return chance
+end
+
+local function parvusTrafficSetupAggression(id)
+    local targetVeh = traffic()[id]
+
+    if random() > probabilityWithinValue(trafficAmount(true), tAggresion.startchance, tAggresion.decay, tAggresion.threshold) then
+        -- Aggression
+        local res = 10 ^ tAggresion.resolution -- [0,1,2,3,4,5,6] allowed resolutions
+        local maxr = tAggresion.maxAggression
+        local minr = trafficVars().baseAggression or tAggresion.baseAggression
+        local aggression = (
+            ((random(0, res) / res) ^ tAggresion.skew) * (maxr - minr) + minr
+        ) -- lower skew between 0.35 and 2
+
+        local function roleSetAggression(c, a)
+            local veh = traffic()[id]
+            c = c or 0
+            if c > 1 then return end
+            if veh and veh.role then
+                veh.role.driver.aggression = aggression
+                log('D', logTag, '(' .. id .. ') Set Role Aggression: (' .. a .. ')')
+                return
+            end
+            log('D', logTag, '(' .. id .. ') Failed to set aggression: (' .. a .. ')')
+        end
+
+        -- self.queuedFuncs = {}  keys: timer, func, args, vLua (vLua string overrides func and args)
+        roleSetAggression(0, aggression)
+        targetVeh.queuedFuncs.parvusTrafficSetAggression = {
+            timer = 0.25,
+            vLua = 'ai.setAggression(' .. aggression .. ')'
+        }
+        log('D', logTag, '(' .. id .. ') Queued Aggression: (' .. aggression .. ')')
+
+
+        -- Tougher Driver
+        local damageLimits = targetVeh.damageLimits
+        if aggression > tToughness.aggressionThreshold and damageLimits and random() > probabilityWithinValue(trafficAmount(true), tToughness.startchance, tToughness.decay, tToughness.threshold) then
+            log('D', logTag, '(' .. id .. ') Tougher Driver Spawned (Aggression=' .. aggression .. ')')
+            for i, v in ipairs(damageLimits) do
+                targetVeh.damageLimits[i] = max(v, floor(v * aggression))
+            end
+            log('D', logTag, 'Damage Limits: (' .. dumps(damageLimits) .. ')')
+        end
+
+        -- Outlaws
+        if aggression > tOutlaw.aggressionThreshold and random() > probabilityWithinValue(trafficAmount(true), tOutlaw.startchance, tOutlaw.decay, tOutlaw.threshold) then
+            targetVeh.queuedFuncs.parvusTrafficsetSpeedMode = {
+                timer = 0.25,
+                vLua = 'ai.setSpeedMode("off")',
+            }
+            log('D', logTag, '(' .. id .. ') Outlaw Spawned (Aggression=' .. aggression .. ')')
+        end
+    end
 end
 
 -- when a vehicle is reset
 local function onVehicleResetted(id)
     -- Only During Traffic
     if trafficGetState() ~= "on" then return end
-    local trafficVeh = traffic()[id]
-    if trafficVeh and trafficVeh.isAi then
-        -- Past threshold the chances begin to drop
-        local function probabilityWithinValue(value, startChance, decay, threshold)
-            -- Get amount of active traffic vehicles
-            local N = value
-
-            local chance
-            if N <= threshold then
-                chance = startChance
-            else
-                chance = startChance / (1 + decay * (N - threshold))
-            end
-
-            return chance
-        end
-
-        if random() > probabilityWithinValue(trafficAmount(true), tAggresion.startchance, tAggresion.decay, tAggresion.threshold) then
-            -- Vehicle Object
-            local obj = getObjectByID(id)
-
-            -- Aggression
-            local res = 10 ^ tAggresion.resolution -- [0,1,2,3,4,5,6] allowed resolutions
-            local maxr = tAggresion.maxAggression
-            local minr = trafficVars().baseAggression or tAggresion.baseAggression
-            local aggression = (
-                ((random(0, res) / res) ^ tAggresion.skew) * (maxr - minr) + minr
-            ) -- lower skew between 0.35 and 2
-            log('D', logTag, '(' .. id .. ') Set Aggression: (' .. aggression .. ')')
-            obj:queueLuaCommand('ai.setAggression(' .. aggression .. ')')
-
-            -- Tougher Driver
-            local damageLimits = trafficVeh.damageLimits
-            if aggression > tToughness.aggressionThreshold and damageLimits and random() > probabilityWithinValue(trafficAmount(true), tToughness.startchance, tToughness.decay, tToughness.threshold) then
-                log('D', logTag, '(' .. id .. ') Tougher Driver Spawned (Aggression=' .. aggression .. ')')
-                for i = 1, #damageLimits do
-                    damageLimits[i] = floor(damageLimits[i] * min(1, aggression))
-                end
-                log('D', logTag, 'Damage Limits: (' .. dumps(damageLimits) .. ')')
-            end
-
-            -- Outlaws
-            if aggression > tOutlaw.aggressionThreshold and random() > probabilityWithinValue(trafficAmount(true), tOutlaw.startchance, tOutlaw.decay, tOutlaw.threshold) then
-                obj:queueLuaCommand('ai.setSpeedMode("off")')
-                log('D', logTag, '(' .. id .. ') Outlaw Spawned (Aggression=' .. aggression .. ')')
-            end
-        end
+    local targetVeh = traffic()[id]
+    if targetVeh and targetVeh.isAi then
+        parvusTrafficSetupAggression(id)
     end
 end
 
@@ -197,10 +372,12 @@ end
 
 local function onTrafficVehicleRemoved(id)
     trafficIdsSorted = tableKeysSorted(traffic())
+    parvusTrafficResetAllBoards()
 end
 
 -- parvusTraffic interface
 M.parvusTrafficResetAllBoards = parvusTrafficResetAllBoards
+M.parvusTrafficSetupAggression = parvusTrafficSetupAggression
 
 -- interface
 M.onUpdate = onUpdate
